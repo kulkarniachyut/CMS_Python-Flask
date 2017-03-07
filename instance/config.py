e315ce63dd46b3bac5c23eaae452f84084934c9d
kulkarniachyut <achyut.gk@gmail.com>
2017-03-06 17:16:10 -0800

    initial jwt setup

--- instance/config.py
+++
@@ -1,2 +0,0 @@
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ggmunsaf23@localhost/dreamteam_db_flask'
--- test.py
+++
@@ -1,9 +0,0 @@
-def outer():
-    x = 1
-    def inner():
-     print x # 1
-    return inner
-foo = outer()
-foo
-foo.func_closure
-foo()
